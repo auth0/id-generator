441fccd8ff167b814e59d03030ac9b59f1b9e913
dschenkelman <damian.schenkelman@gmail.com>
2015-02-05 15:25:17 -0300

    Basic implementation

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # id-generator
 Generates random ids with a prefix (a la Stripe)
+
+## Installing
+```
+npm i auth0-id-generator
+```
+
+## Using
+Simple case:
+```javascript
+var IdGenerator = require('auth0-id-generator');
+
+var generator = new IdGenerator();
+var id = generator.new('cus');
+
+console.log(id); // cus_lO1DEQWBbQAACfHO
+```
+
+Predefined set of allowed prefixes (to avoid mistakes):
+```javascript
+var IdGenerator = require('auth0-id-generator');
+
+var generator = new IdGenerator(['cus', 'con']);
+var id = generator.new('cus');
+
+console.log(id); // cus_lO1DEQWBbQAACfHO
+
+generator.new('cli'); // throws
+```
+
+## Author
+[Auth0](http://auth0.com)
+
+## License
+This project is licensed under the MIT license. See the [LICENSE](LICENSE) file for more info.
+##
---
+++ test/tests.js
@@ -0,0 +1,36 @@
+var expect  = require('chai').expect;
+var IdGenerator = require('../lib');
+
+describe('with no accepted prefix list', function () {
+  var id_generator;
+  before(function(){
+    id_generator = new IdGenerator();
+  });
+
+  it ('should generate id with provided prefix', function(){
+    var id = id_generator.new('cus');
+    expect(id).to.have.length(20);
+    expect(id).to.match(/^cus_[a-zA-Z0-9]{16}$/);
+  });
+
+  it ('should throw if no prefix is provided', function(){
+    expect(id_generator.new).to.throw(Error, /missing prefix for id/);
+  });
+});
+
+describe('with accepted prefix list', function () {
+  var id_generator;
+  before(function(){
+    id_generator = new IdGenerator(['cus', 'con']);
+  });
+
+  it ('should generate id if prefix is in list', function(){
+    var id = id_generator.new('cus');
+    expect(id).to.have.length(20);
+    expect(id).to.match(/^cus_[a-zA-Z0-9]{16}$/);
+  });
+
+  it ('should throw prefix is not in list', function(){
+    expect(function() { id_generator.new('cli') }).to.throw(Error, /invalid prefix cli, valid: cus,con/);
+  });
+});
