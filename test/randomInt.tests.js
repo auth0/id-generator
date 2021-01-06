c3b9da72232a03d4959d06a67f2a2ce46cc10065
Tom Carter <tom.carter@auth0.com>
2021-01-06 12:39:50 +0000

    simplify randomInteger implementation, explicitly block support for negative numbers - we only support positive ranges.
    clean up decls in index.js

--- test/randomInt.tests.js
+++ test/randomInt.tests.js
@@ -1,37 +1,25 @@
-var expect = require('chai').expect;
+const expect = require('chai').expect;
 const { assert } = require('chai');
-var randomInt = require('../lib/randomInt');
+const randomInteger = require('../lib/randomInteger');
 
-describe("randomInt", function () {
-    it("should throw when min greater than max", function () {
+describe("randomInteger", function () {
+    it("should throw when passed a negative value", function () {
         expect(function () {
-            randomInt(10, 0);
-        }).to.throw(Error, /must be greater than the value of "min"/);
+            randomInteger(-10);
+        }).to.throw(Error, /must be a natural number/);
     });
 
-    it("should throw when passed and unsafe integer", function () {
+    it("should throw when passed an unsafe integer", function () {
         expect(function () {
-            randomInt(Number.MAX_VALUE);
+            randomInteger(Number.MAX_VALUE);
         }).to.throw(Error, /must be a safe integer/);
     });
 
-    it("should return a random number within the specified range", function () {
+    it("should return a random number less than the specified maximum and greater than 0", function () {
         let randomInts = [];
 
         for (let i = 0; i <= 1000; i++) {
-            randomInts.push(randomInt(5, 10));
-        }
-        randomInts.forEach(e => {
-            assert.isAtMost(e, 10);
-            assert.isAtLeast(e, 5);
-        });
-    });
-
-    it("should return a random number less than the specified maximum and great than 0", function () {
-        let randomInts = [];
-
-        for (let i = 0; i <= 1000; i++) {
-            randomInts.push(randomInt(16));
+            randomInts.push(randomInteger(16));
         }
         randomInts.forEach(e => {
             assert.isAtMost(e, 16);
